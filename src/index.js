d7b8b2eb894534568409ab9fcd0cc27e2753f6f2
Daniel Quispe <v-daquis@microsoft.com>
2021-08-25 22:52:23 -0500

    babel installed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello world!")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const app = 
+ReactDOM.render(<h1>Hey there PetGram!</h1>, document.getElementById('app'))
